f0a839595ce2552b8c801cbe186d5a9956324e57
ktc <>
2026-07-31 10:40:48 +0900

    パッチ４

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,12 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 3;
     [SerializeField] float rotateSpeed = 20;
+    [SerializeField] int hp = 2;
+    [SerializeField] float invincibleTimeMax = 0.5f;
+    [SerializeField] float knockbackSpeed = 5;
 
     Rigidbody rb;
+    float invincibleTime = 0;
 
     public Collider playerCollider { get; set; }
 
@@ -31,7 +35,7 @@ public class Enemy : MonoBehaviour
             }
         }
 
-        if (isSeenPlayer)
+        if (isSeenPlayer && invincibleTime <= 0)
         {
             var subVec = playerCollider.bounds.center - rb.position;
             subVec.y = 0;
@@ -43,6 +47,32 @@ public class Enemy : MonoBehaviour
             Vector3 forward = transform.forward;
             transform.forward = Vector3.Slerp(forward, rotateTarget,
                 rotateSpeed * Time.deltaTime);
+        }
+
+        // 無敵時間を減らす
+        if(invincibleTime > 0)
+        {
+            invincibleTime -= Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        var attackObj = collision.gameObject.GetComponent<AttackObject>();
+        if (attackObj != null && invincibleTime <= 0)
+        {
+            hp -= attackObj.power;
+            invincibleTime = invincibleTimeMax;
+            if (hp <= 0)
+            {
+                Destroy(gameObject);
+            }
+
+            // ノックバック
+            var dir = transform.position - collision.transform.position;
+            dir.y = 0;
+            var knockbackVec = dir.normalized * knockbackSpeed;
+            rb.linearVelocity = knockbackVec;
         }
     }
 }
